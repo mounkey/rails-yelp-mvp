c5721a2838d806ce0e6177eaa3795785d7c0e341
juan grover <jpgrover@gmail.com>
2021-08-16 16:11:58 -0400

    Listo desafio

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+catego = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Eliminando toda la base de datos:"
+Restaurant.destroy_all
+Review.destroy_all
+
+10.times do
+  bristol = Restaurant.new(name: Faker::Restaurant.name , category: catego.sample, phone_number: Faker::PhoneNumber.extension, address: Faker::Address.city)
+  bristol.save                # Insert into DB and set id
+  yummy = Review.new(content: Faker::Quote.yoda, rating: rand(0..5))
+  yummy.restaurant = bristol  # Set foreign key restaurant_id
+  yummy.save
+  bristol.reviews             # Should contain the yummy review
+  yummy.restaurant
+end
