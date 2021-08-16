de68dca050ac498bd9031c7b7c339b7bbbc54af4
juan grover <jpgrover@gmail.com>
2021-08-16 17:22:57 -0400

    listo desafio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reviews, only: [ :new, :create]
+  resources :restaurants, except: [:update, :edit ] do
+    resources :reviews, only: [:create]
   end
 end
